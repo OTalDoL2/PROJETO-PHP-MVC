27df95b61154a574fa519de6e5e2aaa5f0482870
Lucas M <56313048+OTalDoL2@users.noreply.github.com>
2023-12-03 23:47:44 -0300

    iniciando o projeto

---
+++ Core/Router.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Core;
+
+class Router{
+    private $controller = 'Login';
+    private $method = 'index';
+    private $param = '';
+    public function __construct(){
+        // 3 Informações importantes do url:
+        // - Nome da classe chamada
+        // - Nome do metodo
+        // - Nome do parametro
+        
+        $router = $this->url();
+        
+        if(file_exists('App/Controller/' . ucfirst($router[0]) . '.php')){
+            $this->controller = $router[0];
+            unset($router[0]);
+        }
+        
+        $class = "\\App\\Controllers\\" . ucfirst($this->controller);
+        $object = new $class;
+
+        if(isset($router[1]) and method_exists($class, $router[1])){
+            $this->method = $router[1];
+            unset( $router[1]);
+        }
+
+        $this->param = $router ? array_values( $router ) : [];
+
+        call_user_func_array([$object, $this->method], $this->param);
+    }
+
+    public function url(){
+        $url = explode('/', filter_input(INPUT_GET, 'router', FILTER_SANITIZE_URL));
+        return $url;
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// importando o Router
+use Core\Router;
+
+// Require once busca um arquivo, e o DIR diz que a busca se iniciará na raiz 
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+// Criando o router
+$router = new Router;
+
