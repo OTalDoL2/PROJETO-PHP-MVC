04a98f5976e1b2b91a05b317c769acf328423f7c
Rafael Santiago <108771464+SantiagoRafael1@users.noreply.github.com>
2023-12-14 21:11:21 -0300

    Sobre V1

---
+++ App/Views/sobre.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sobre</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+              body {
+          padding-top: 3rem;
+          padding-bottom: 3rem;
+          color: #5a5a5a;
+          background-color: #f1f1f1;
+        }
+        .marketing .col-lg-4 {
+          margin-bottom: 1.5rem;
+          text-align: center;
+        }
+        .marketing h2 {
+          font-weight: 400;
+        }
+        .marketing .col-lg-4 p {
+          margin-right: .75rem;
+          margin-left: .75rem;
+        }
+        .featurette-divider {
+          margin: 5rem 0; 
+        }
+        .featurette-heading {
+          font-weight: 300;
+          line-height: 1;
+          letter-spacing: -.05rem;
+        }
+        @media (min-width: 40em) {
+          /* Bump up size of carousel content */
+          .carousel-caption p {
+            margin-bottom: 1.25rem;
+            font-size: 1.25rem;
+            line-height: 1.4;
+          }
+          .featurette-heading {
+            font-size: 50px;
+          }
+        }
+        @media (min-width: 62em) {
+          .featurette-heading {
+            margin-top: 7rem;
+          }
+        }
+        .custom-move {
+        margin-top: 100px !important;
+        }
+    </style>
+
+  </head>
+  <body>
+    
+<header>
+  <nav class="navbar navbar-expand-md navbar-light fixed-top" style="background-color: #ffffff;">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <!-- Aqui você pode adicionar a sua imagem -->
+            <img src="https://i.postimg.cc/wjQk8dwr/gsaude.png" alt="Logo" width="70" height="70" class="rounded-circle">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" style="font-weight: bold;" aria-current="page" href="#">Marcar Exame</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="font-weight: bold;" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" style="font-weight: bold;" href="#">Sobre</a>
+              </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+</header>
+
+<main>
+
+
+  <!-- CARROSEL À CIMA
+  ================================================== -->
+  <!--  -->
+
+  <div class="container marketing mt-5 custom-move">
+
+    
+    <div class="row">
+      <div class="col-lg-4">
+        <img src="https://i.postimg.cc/QMdf2LKQ/IMG-1542.jpg" alt="Placeholder 140x140" class="bd-placeholder-img rounded-circle" width="140" height="140">
+
+        <h2>Lucas Ramos</h2>
+        <p>Desenvolvedor Full Stack</p>
+        <p>Com uma ampla experiência no desenvolvimento de soluções web, Lucas Ramos traz consigo habilidades técnicas excepcionais.</p>
+        <p><a class="btn btn-secondary bg-primary" href="https://github.com/OTalDoL2" target="_blank">Exibir &raquo;</a></p>
+      </div>
+      <div class="col-lg-4">
+        <img src="https://i.postimg.cc/8PbBNKSt/IMG-8229.jpg" alt="Placeholder 140x140" class="bd-placeholder-img rounded-circle" width="140" height="140">
+
+        <h2>Rafael Santiago</h2>
+        <p>Desenvolvedor Full Stack</p>
+        <p>Rafael Santiago é um profissional apaixonado por desenvolvimento de software. Com expertise em arquitetura de sistemas, ele busca constantemente soluções eficientes.</p>
+        <p><a class="btn btn-secondary bg-primary" href="https://github.com/SantiagoRafael1" target="_blank">Exibir &raquo;</a></p>
+    </div>
+      <div class="col-lg-4">
+        <img src="https://i.postimg.cc/pLZ0cVrV/IMG-1541.jpg" alt="Placeholder 140x140" class="bd-placeholder-img rounded-circle" width="140" height="140">
+
+        <h2>Rodrigo Alves</h2>
+        <p>Desenvolvedor Full Stack</p>
+        <p>Rodrigo é especializado no desenvolvimento de interfaces de usuário impressionantes. Sua paixão pela estética e usabilidade resulta em experiências digitais envolventes e intuitivas.</p>
+        <p><a class="btn btn-secondary bg-primary" href="https://github.com/Rodrigoadb" target="_blank">Exibir &raquo;</a></p>
+      </div>
+    </div>
+
+
+    <!-- FEATURETTES -->
+
+
+  <!-- FOOTER -->
+  <!-- FOOTER -->
+<footer class="container text-center">
+    <p>&copy; PROJETO-PHP-MVC ADS3B &middot; <a href="https://github.com/OTalDoL2/PROJETO-PHP-MVC/tree/main">Github</a> &middot;
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+      
+  </body>
+</html>
