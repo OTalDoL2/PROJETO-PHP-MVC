39bffa707e120cdc0f73058ddbe24d17288418f6
Lucas M <56313048+OTalDoL2@users.noreply.github.com>
2023-12-15 19:41:21 -0300

    Update cadastro.php

--- App/Views/cadastro.php
+++ App/Views/cadastro.php
@@ -23,7 +23,7 @@
         }
 
         .custom-form-group {
-            margin-right: 32rem; /* Ajuste conforme necessário */
+            margin-right: 32rem;
         
         }
         .custom-checkbox {
@@ -73,7 +73,7 @@
                     <div class="row justify-content-md-center ">
                         <div class="card-wrapper text-center col-md-7">
                             <div class="brand">
-                                <!-- Seu conteúdo de marca vai aqui -->
+                               
                             </div>
                             <div class="card fat shadow-lg p-3 bg-white rounded-start">
                                 <div class="card-body">
@@ -132,7 +132,7 @@
             </section>
         
             
-            <!--<script src="config/materialize/js/materialize.min.js"></script>!-->
+            
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
